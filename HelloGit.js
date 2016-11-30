6287b870278d1702a055f764b75a5670db84290a
mageets <mageets@hotmail.com>
2016-11-30 15:06:55 +0000

    simple person looping

--- HelloGit.js
+++ HelloGit.js
@@ -1,1 +1,35 @@
-console.log("hello git! from paul   ");
+var people = [
+    {'name':'Paul', 'age':34}, 
+    {'name':'Karen', 'age':34},
+    {'name':'Neal', 'age':39},
+    {'name':'Cormac', 'age':35},
+    {'name':'Edward', 'age':29}
+];
+// people.forEach(function(element) {
+//     console.log('Hello ' + element.name + ' You are ' + element.age);
+// }, this);
+
+var totalNumberOfPeople = 0;
+var totalAge = 0;
+
+people.forEach(function(person) {
+    console.log('Hello. Here are your details...')
+    for (var prop in person) {
+        console.log(prop + ':' + person[prop]);
+        if (prop === 'age') {
+            totalAge += person[prop];
+        }
+    }
+    console.log('\n');
+    totalNumberOfPeople++;
+});
+console.log(totalNumberOfPeople + ' people, with a combined age of ' + totalAge);
+
+// var person = {name:'Paul', age:34};
+// for (var key in person) {
+//     console.log(person[key]);
+// }
+
+
+
+
