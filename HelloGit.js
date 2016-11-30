113643846c80387a06a1af57962d3e12d51ae70a
mageets <mageets@hotmail.com>
2016-11-30 15:26:45 +0000

    updated to use prototyping

--- HelloGit.js
+++ HelloGit.js
@@ -1,25 +1,31 @@
+function Person(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+}
+
+Person.prototype.fullName = function() {
+    return this.firstName + ' ' + this.lastName;
+}
+
 var people = [
-    {'name':'Paul', 'age':34}, 
-    {'name':'Karen', 'age':34},
-    {'name':'Neal', 'age':39},
-    {'name':'Cormac', 'age':35},
-    {'name':'Edward', 'age':29}
+    new Person('Paul','Magee',34), 
+    new Person('Karen','Gallagher',34), 
+    new Person('Neal','Simpson',39),
+    new Person('Cormac','O\'Dowd',35), 
+    new Person('Paul','Kelly',35), 
 ];
 
 var totalNumberOfPeople = 0;
 var totalAge = 0;
 
 people.forEach(function(person) {
-    console.log('Hello. Here are your details...')
-    for (var prop in person) {
-        console.log(prop + ':' + person[prop]);
-        if (prop === 'age') {
-            totalAge += person[prop];
-        }
-    }
-    console.log('\n');
+    console.log('Name: ' + person.fullName() + ', age:' + person.age);
     totalNumberOfPeople++;
+    totalAge += person.age;
 });
+
+console.log('\n');
 console.log(totalNumberOfPeople + ' people, with a combined age of ' + totalAge);
 
 
